64fb1a7a26d6443aefc0914f7d79b843496ca928
jojomueller05 <j_j_m@bluewin.ch>
2025-03-07 15:29:35 +0100

    tried fixing urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import alpinejs from "@astrojs/alpinejs";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jojomueller05.github.io',
-  base: 'jojomueller05.github.io',
+  base: 'portfolio',
   vite: {
       plugins: [tailwindcss()],
   },
